6329b3134acc24e12831aec0d4c9b4309f6e6362
Benjamin Horner <benjamin.horner@socianova.com>
2015-09-10 14:53:59 +0200

    Initial commit

---
+++ QandaRecorder/QandaRecorder.h
@@ -0,0 +1,19 @@
+//
+//  QandaRecorder.h
+//  QandaRecorder
+//
+//  Created by Benjamin Horner on 10/09/2015.
+//  Copyright (c) 2015 Qanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QandaRecorder.
+FOUNDATION_EXPORT double QandaRecorderVersionNumber;
+
+//! Project version string for QandaRecorder.
+FOUNDATION_EXPORT const unsigned char QandaRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QandaRecorder/PublicHeader.h>
+
+
